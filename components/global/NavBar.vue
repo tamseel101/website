8cdea10991d5e2a84b6c96444dcb1ff8589e50be
Chris <hello@hiimchrislim.co>
2020-08-31 11:15:14 -0400

    Add navbar progress

---
+++ components/global/NavBar.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="nav g-header p-3">
+    <div class="nav-header">
+      <nuxt-link
+        class="d-flex align-items-center ml-1"
+        to="/"
+      >
+        <img
+          class="logo"
+          src="../../static/logos/cssc.png"
+        />
+        <span class="logo-text">UTM CSSC</span>
+      </nuxt-link>
+    </div>
+    <div class="nav-links">
+      <nuxt-link
+        to="/resources"
+        class="g-nav-link mr-4"
+      >Resources
+      </nuxt-link>
+      <nuxt-link
+        to="/ask-an-exec"
+        class="g-nav-link mr-4"
+      >Ask an Exec
+      </nuxt-link>
+      <nuxt-link
+        to="/tech-of-the-month"
+        class="g-nav-link mr-4"
+      >Tech of the month
+      </nuxt-link>
+      <a
+        target="_blank"
+        href="https://www.github.com/utm-cssc"
+        class="g-nav-link mr-4"
+      >Github</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .g-header {
+    height: 8vh;
+  }
+
+  .logo {
+    height: 48px;
+    width: 48px;
+    margin-right: 16px;
+  }
+
+  .logo-text {
+    font-size: 26px;
+    color: var(--color-text);
+    font-weight: 500;
+    text-transform: uppercase;
+  }
+
+  .nav > .nav-links {
+    float: right;
+    display: flex;
+    margin-left: auto;
+    font-size: 20px;
+  }
+
+  .nav > .nav-header {
+    display: inline-block;
+    text-decoration: none;
+  }
+
+</style>
+<style>
+  .nav {
+    width: 100%;
+    position: relative;
+    border-bottom: 1px solid rgba(232, 231, 231, 0.97);
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,30 @@
 <template>
   <div>
+    <NavBar />
     <Nuxt />
   </div>
 </template>
 
 <script>
-// /* eslint-disable */
+// eslint-disable
 // export default {
 //   created () {
-//     // eslint-disable-next-line
-//     document.addEventListener('keyup', (e) => {
-//       if (e.key === 'd') {
-//         // eslint-disable-next-line
-//         [].forEach.call(document.querySelectorAll('*'), function (a) {
-//           a.style.outline =
-//             '1px solid #' + (~~(Math.random() * (1 << 24))).toString(16)
-//         })
-//       }
-//     })
-//   }
+// eslint - disable - next - line
+// document.addEventListener('keyup', (e) => {
+//   if (e.key === 'd') {
+// eslint - disable - next - line
+// [].forEach.call(document.querySelectorAll('*'), function (a) {
+//   a.style.outline =
+//     '1px solid #' + (~~(Math.random() * (1 << 24))).toString(16)
+// })
+// }
+// })
+// }
 // }
 </script>
+
+<style>
+  :root {
+    --color-text: #00d097;
+  }
+</style>
